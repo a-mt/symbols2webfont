183780d2dec53be86a8252fb467b74dc7a13694c
a-mt <amistri@live.fr>
2018-08-07 04:36:50 +0200

    Create svg webfont from list of svg files (.zip)

--- inc.tpl.php
+++ inc.tpl.php
@@ -9,16 +9,21 @@
             margin: 0;
             margin-bottom: 5px;
         }
+        h2 {
+            margin: 0;
+            font-size: 1em;
+        }
         div[hidden] {
             display: none;
         }
-        #about {
+        .about {
             background: #f5f5f5;
             border: 1px solid #ccc;
             padding: 5px;
             padding-bottom: 0;
+            margin-top: 10px;
         }
-        #info {
+        .info {
             background: none;
             border: 0;
             cursor: pointer;
@@ -34,6 +39,11 @@
             border-radius: 15px;
             padding: 15px;
             margin: 5px 0;
+            background: url(dust_scratches.png);
+        }
+        fieldset {
+            margin: 20px 0;
+            background: white;
         }
         .file {
             margin-top: 20px;
@@ -79,6 +89,9 @@
             background-color: #fcf8e3;
             border-color: #faebcc;
         }
+        button {
+          cursor: pointer;
+        }
     </style>
   </head>
   <body>
@@ -101,34 +114,53 @@
     <?php } ?>
 
     <form method="POST" enctype="multipart/form-data">
-      <h1>SVG Symbols &rarr; SVG Webfont
-            <button type="button" id="info" onclick="toggleAbout()">&#9432;</button>
-      </h1>
-      <div id="about" hidden>
-        Upload a SVG File of Symbols:
-        <pre>&lt;svg xmlns=&quot;http://www.w3.org/2000/svg&quot; xmlns:xlink=&quot;http://www.w3.org/1999/xlink&quot;
-     style=&quot;position: absolute; width: 0; height: 0&quot; id=&quot;__SVG_SPRITE_NODE__&quot;&gt;
-&lt;symbol viewBox=&quot;0 0 8 12&quot; id=&quot;myicon&quot;&gt;
-  &lt;path fill-rule=&quot;evenodd&quot; d=&quot;M5.982 0c-.06.01-.112.06-.123.074-.007.007-.02.014-.022.02L.04 6.613c-.025.027-.04.063-.04.105 0 .083.063.148.144.148h2.95l-1.23 4.926v.033c-.002.01-.01.02-.01.03 0 .084.063.15.144.15.045 0 .085-.02.112-.054l5.848-6.854c.027-.028.04-.064.04-.105 0-.082-.06-.147-.142-.147H4.69L6.105.223c.01-.02.01-.04.01-.064 0-.085-.004-.12-.07-.15C6.023.004 6 0 5.98 0&quot;&gt;&lt;/path&gt;
-&lt;/symbol&gt;&lt;/svg&gt;</pre>
-        Retrieve a SVG Webfont:
-        <pre>&lt;?xml version=&quot;1.0&quot; standalone=&quot;no&quot;?&gt;
-&lt;!DOCTYPE svg PUBLIC &quot;-//W3C//DTD SVG 1.1//EN&quot; &quot;http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd&quot;&gt;
-&lt;svg xmlns=&quot;http://www.w3.org/2000/svg&quot;&gt;
-   &lt;defs&gt;
-      &lt;font id=&quot;SVGFont&quot; horiz-adv-x=&quot;512&quot;&gt;
-         &lt;font-face units-per-em=&quot;512&quot; ascent=&quot;448&quot; descent=&quot;-64&quot; x-height=&quot;240&quot; cap-height=&quot;480&quot;/&gt;
-         &lt;missing-glyph horiz-adv-x=&quot;512&quot;/&gt;
-         &lt;glyph unicode=&quot;57345&quot; glyph-name=&quot;myicon&quot; horiz-adv-x=&quot;341&quot; d=&quot;M255.232 448c-2.56 -0.42667 -4.77867 -2.56 -5.248 -3.15733 -0.29867 -0.29867 -0.85333 -0.59733 -0.93867 -0.85333L1.70667 165.84533c-1.06667 -1.152 -1.70667 -2.688 -1.70667 -4.48 0 -3.54133 2.688 -6.31467 6.144 -6.31467h125.86667l-52.48 -210.176v-1.408c-0.08533 -0.42667 -0.42667 -0.85333 -0.42667 -1.28 0 -3.584 2.688 -6.4 6.144 -6.4 1.92 0 3.62667 0.85333 4.77867 2.304l249.51467 292.43733c1.152 1.19467 1.70667 2.73067 1.70667 4.48 0 3.49867 -2.56 6.272 -6.05867 6.272H200.10667L260.48 438.48533c0.42667 0.85333 0.42667 1.70667 0.42667 2.73067 0 3.62667 -0.17067 5.12 -2.98667 6.4C256.98133 447.82933 256 448 255.14667 448&quot;/&gt;
-      &lt;/font&gt;
-   &lt;/defs&gt;
-&lt;/svg&gt;</pre>
-      </div>
+      <h1>Generate SVG Webfont</h1>
 
-      <div class="file">
-        <label for="file">File</label>
-        <input id="file" name="file" type="file">
-      </div>
+      <fieldset>
+        <h2>From SVG File of Symbols
+          <button type="button" class="info" onclick="toggleAbout()">&#9432;</button>
+        </h2>
+        <div class="about" hidden>
+          Upload a SVG File of Symbols:
+          <pre>&lt;svg xmlns=&quot;http://www.w3.org/2000/svg&quot; xmlns:xlink=&quot;http://www.w3.org/1999/xlink&quot;
+       style=&quot;position: absolute; width: 0; height: 0&quot; id=&quot;__SVG_SPRITE_NODE__&quot;&gt;
+  &lt;symbol viewBox=&quot;0 0 8 12&quot; id=&quot;myicon&quot;&gt;
+    &lt;path fill-rule=&quot;evenodd&quot; d=&quot;M5.982 0c-.06.01-.112.06-.123.074-.007.007-.02.014-.022.02L.04 6.613c-.025.027-.04.063-.04.105 0 .083.063.148.144.148h2.95l-1.23 4.926v.033c-.002.01-.01.02-.01.03 0 .084.063.15.144.15.045 0 .085-.02.112-.054l5.848-6.854c.027-.028.04-.064.04-.105 0-.082-.06-.147-.142-.147H4.69L6.105.223c.01-.02.01-.04.01-.064 0-.085-.004-.12-.07-.15C6.023.004 6 0 5.98 0&quot;&gt;&lt;/path&gt;
+  &lt;/symbol&gt;&lt;/svg&gt;</pre>
+          Retrieve a SVG Webfont:
+          <pre>&lt;?xml version=&quot;1.0&quot; standalone=&quot;no&quot;?&gt;
+  &lt;!DOCTYPE svg PUBLIC &quot;-//W3C//DTD SVG 1.1//EN&quot; &quot;http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd&quot;&gt;
+  &lt;svg xmlns=&quot;http://www.w3.org/2000/svg&quot;&gt;
+     &lt;defs&gt;
+        &lt;font id=&quot;SVGFont&quot; horiz-adv-x=&quot;512&quot;&gt;
+           &lt;font-face units-per-em=&quot;512&quot; ascent=&quot;448&quot; descent=&quot;-64&quot; x-height=&quot;240&quot; cap-height=&quot;480&quot;/&gt;
+           &lt;missing-glyph horiz-adv-x=&quot;512&quot;/&gt;
+           &lt;glyph unicode=&quot;57345&quot; glyph-name=&quot;myicon&quot; horiz-adv-x=&quot;341&quot; d=&quot;M255.232 448c-2.56 -0.42667 -4.77867 -2.56 -5.248 -3.15733 -0.29867 -0.29867 -0.85333 -0.59733 -0.93867 -0.85333L1.70667 165.84533c-1.06667 -1.152 -1.70667 -2.688 -1.70667 -4.48 0 -3.54133 2.688 -6.31467 6.144 -6.31467h125.86667l-52.48 -210.176v-1.408c-0.08533 -0.42667 -0.42667 -0.85333 -0.42667 -1.28 0 -3.584 2.688 -6.4 6.144 -6.4 1.92 0 3.62667 0.85333 4.77867 2.304l249.51467 292.43733c1.152 1.19467 1.70667 2.73067 1.70667 4.48 0 3.49867 -2.56 6.272 -6.05867 6.272H200.10667L260.48 438.48533c0.42667 0.85333 0.42667 1.70667 0.42667 2.73067 0 3.62667 -0.17067 5.12 -2.98667 6.4C256.98133 447.82933 256 448 255.14667 448&quot;/&gt;
+        &lt;/font&gt;
+     &lt;/defs&gt;
+  &lt;/svg&gt;</pre>
+        </div>
+        <div class="file">
+          <label for="svgfile">SVG File</label>
+          <input id="svgfile" name="svg" type="file">
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <h2>From SVG Files
+          <button type="button" class="info" onclick="toggleAbout()">&#9432;</button>
+        </h2>
+        <div class="about" hidden>
+          Upload a ZIP File containing your files:
+          <pre>myfile.zip
+    myicon.svg</pre>
+        </div>
+        <div class="file">
+          <label for="zipfile">ZIP File</label>
+          <input id="zipfile" name="zip" type="file">
+        </div>
+      </fieldset>
+
       <div class="actions">
           <input type="submit" class="btn">
       </div>
@@ -136,16 +168,19 @@
 
     <script>
         (function(){
-            var infoElement  = document.getElementById("info"),
-                aboutElement = document.getElementById("about");
+            var infoElements = document.getElementsByClassName("info");
 
-            infoElement.addEventListener("click", function(){
-                if(aboutElement.hasAttribute("hidden")) {
-                    aboutElement.removeAttribute("hidden");
-                } else {
-                    aboutElement.setAttribute("hidden", true);
-                }
-            });
+            for(var i=0; i<infoElements.length; i++) {
+              infoElements[i].addEventListener("click", function(){
+                  var aboutElement = this.parentNode.nextElementSibling;
+
+                  if(aboutElement.hasAttribute("hidden")) {
+                      aboutElement.removeAttribute("hidden");
+                  } else {
+                      aboutElement.setAttribute("hidden", true);
+                  }
+              });
+            }
         })();
     </script>
   </body>
--- index.php
+++ index.php
@@ -15,33 +15,62 @@ if(isset($_GET['file']) && isset($_SESSION['file'])) {
 }
 
 // Upload SVG
-if(isset($_FILES['file'])) {
-    $err = "";
+if(isset($_FILES['svg'])) {
 
-    // Check upload
-    switch($_FILES['file']['error']) {
-        case UPLOAD_ERR_OK: break;
-        case UPLOAD_ERR_INI_SIZE: $err = 'Filesize is bigger than ' . ini_get('upload_max_filesize'); break;
-        case UPLOAD_ERR_FORM_SIZE: $err = 'Filesize is bigger than the maximum authorized'; break;
-        case UPLOAD_ERR_PARTIAL: $err = 'The file could not be downloaded'; break;
-        case UPLOAD_ERR_NO_FILE: $err = 'No file uploaded'; break;
-        default: $err = 'The server has encountered an internal error'; break;
-    }
-    if($err) {
-        redirect($err);
-    }
+    // SVG Glyphs to SVG Webfont
+    if($_FILES['svg']['name']) {
+        $err = "";
 
-    // Check file
-    $mime = mime_content_type($_FILES['file']['tmp_name']);
-    if($mime != 'image/svg') {
-        redirect("You should upload a .svg file");
-    }
+        // Check upload
+        $err = getErrorMsg($_FILES['svg']['error']);
+        if($err) {
+            redirect($err);
+        }
 
-    $txt = file_get_contents($_FILES['file']['tmp_name']);
-    if($txt) {
+        // Check file
+        $mime = mime_content_type($_FILES['svg']['tmp_name']);
+        if($mime != 'image/svg') {
+            redirect("You should upload a .svg file");
+        }
 
-        // Convert to webfont 
-        list($err, $output) = convert($txt);
+        $txt = file_get_contents($_FILES['svg']['tmp_name']);
+        if($txt) {
+
+            // Convert to webfont
+            list($err, $output) = convertGlyphs($txt);
+
+            if($output) {
+                $_SESSION["file"]     = $output;
+                $_SESSION["download"] = 1;
+                redirect(null, $err);
+            } else {
+                redirect($err);
+            }
+        }
+
+    // ZIP of svg files
+    } else if($_FILES['zip']['name']) {
+        $err = "";
+
+        // Check upload
+        $err = getErrorMsg($_FILES['zip']['error']);
+        if($err) {
+            redirect($err);
+        }
+
+        // Check file
+        $mime = mime_content_type($_FILES['zip']['tmp_name']);
+        if($mime != 'application/zip') {
+            redirect("You should upload a .zip file");
+        }
+        $zip = new ZipArchive;
+        if(!$zip->open($_FILES['zip']['tmp_name'])) {
+            redirect("Could not open zip file");
+        }
+
+        // Convert to webfont
+        list($err, $output) = convertFiles($zip);
+        zip_close($zip);
 
         if($output) {
             $_SESSION["file"]     = $output;
@@ -53,6 +82,17 @@ if(isset($_FILES['file'])) {
     }
 }
 
+function getErrorMsg($errorCode) {
+    switch($errorCode) {
+        case UPLOAD_ERR_OK: return;
+        case UPLOAD_ERR_INI_SIZE: return 'Filesize is bigger than ' . ini_get('upload_max_filesize');
+        case UPLOAD_ERR_FORM_SIZE: return 'Filesize is bigger than the maximum authorized';
+        case UPLOAD_ERR_PARTIAL: return 'The file could not be downloaded';
+        case UPLOAD_ERR_NO_FILE: return 'No file uploaded';
+        default: return 'The server has encountered an internal error';
+    }
+}
+
 function redirect($err = null, $warning = null) {
     $_SESSION["error"]   = $err;
     $_SESSION["warning"] = $warning;
@@ -61,7 +101,7 @@ function redirect($err = null, $warning = null) {
 }
 
 // Convert SVG symbols to SVG Webfont
-function convert($txt) {
+function convertGlyphs($txt) {
     libxml_use_internal_errors(true);
 
     require __DIR__ . '/SVG-Icon-Font-Generator/Document.php';
@@ -100,17 +140,74 @@ function convert($txt) {
             $code    = hexdec('e001') + $n;
 
             try {
-            	$font->addGlyph(
-				    $code,
-				    $iconDoc->getPath($fontOptions['units-per-em']/$viewBox['height'], 5, 'vertical', true, 0, $fontOptions['descent']),
-				    $id,
-				    round($viewBox['width']*$fontOptions['units-per-em']/$viewBox['height'])
-			    );		    
+                $font->addGlyph(
+                    $code,
+                    $iconDoc->getPath($fontOptions['units-per-em']/$viewBox['height'], 5, 'vertical', true, 0, $fontOptions['descent']),
+                    $id,
+                    round($viewBox['width']*$fontOptions['units-per-em']/$viewBox['height'])
+                );
                 $n++;
 
-			} catch(Exception $e){
-			    $err .= "Icon $id: " . $e->getMessage() . "\n";
-			}
+            } catch(Exception $e){
+                $err .= "Icon $id: " . $e->getMessage() . "\n";
+            }
+        }
+    }
+
+    // Return created Webfont
+    if($n) {
+        return [$err, $font->getXML()];
+    } else {
+        return ["Empty file", null];
+    }
+}
+
+
+// Create SVG Webfont form list of svg files
+function convertFiles($zip) {
+    require __DIR__ . '/SVG-Icon-Font-Generator/Document.php';
+    require __DIR__ . '/SVG-Icon-Font-Generator/Font.php';
+    require __DIR__ . '/SVG-Icon-Font-Generator/IconFontGenerator.php';
+
+    $n = 0;
+
+    // Create Webfont
+    $err  = "";
+    $font = new Font();
+    $fontOptions = $font->getOptions();
+
+    // Loop files
+    for($i = 0; $i < $zip->numFiles; $i++){
+        $stat = $zip->statIndex($i);
+
+        // Only handle svg files
+        if(!$stat['size']) {
+            continue;
+        }
+        if(pathinfo($stat['name'], PATHINFO_EXTENSION) != 'svg') {
+            continue;
+        }
+
+        // Get XML
+        $id  = pathinfo($stat['name'], PATHINFO_FILENAME);
+        $xml = $zip->getFromIndex($i);
+
+        // Add glyph to Webfont
+        $iconDoc = new Document($xml);
+        $viewBox = $iconDoc->getViewBox();
+        $code    = hexdec('e001') + $n;
+
+        try {
+            $font->addGlyph(
+                $code,
+                $iconDoc->getPath($fontOptions['units-per-em']/$viewBox['height'], 5, 'vertical', true, 0, $fontOptions['descent']),
+                $id,
+                round($viewBox['width']*$fontOptions['units-per-em']/$viewBox['height'])
+            );
+            $n++;
+
+        } catch(Exception $e){
+            $err .= "Icon $id: " . $e->getMessage() . "\n";
         }
     }
 
